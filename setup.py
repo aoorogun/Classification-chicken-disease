caf22d01aff8c2379a77134784a7cc1d662e03b3
Orogun Adebola <146725180+aoorogun@users.noreply.github.com>
2024-08-15 00:05:50 +0100

    util v0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Chicken-Disease-Classification--Project"
-AUTHOR_USER_NAME = "okunola Orogun"
+AUTHOR_USER_NAME = "aoorogun"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "okunolaorogun@icloud.com"
 
